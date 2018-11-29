b3f9547be8ff7320a40ff44b9b4fc39c5ee30973
jl0128 <43858468+jl0128@users.noreply.github.com>
2018-11-29 11:00:44 -0700

    Add files via upload

---
+++ factorial.s
@@ -0,0 +1,26 @@
+	.arch armv6
+	.text
+	.align	2
+	.global	factorial_efficient
+	.syntax unified
+	.arm
+	.fpu vfp
+	.type	factorial_efficient, %function
+factorial_efficient:
+	@ args = 0, pretend = 0, frame = 16
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	
+	
+	mov r4, r0
+	
+	fin
+	cmp r4, #1
+	bne loop
+	mov pc, #26
+
+	loop
+	sub r4, #1
+	mul r0, r0, r4
+	b fin
+
